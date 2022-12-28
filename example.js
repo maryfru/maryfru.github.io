c2ac30b9f23f9fcebcb718b5bd46fd1bca15c9cc
Mary-Ann Johanna Frunzete <maryfrunzete@gmail.com>
2022-12-28 19:13:08 +0200

    First Version of my CV

---
+++ example.js
@@ -0,0 +1,25 @@
+// add your code here
+console.info("page loaded");
+console.debug(2 * 3 + 1);
+console.warn(2 * (3 + 1));
+
+var employed = true; // boolean
+console.error(employed);
+
+var age = 24; // number
+
+var user = "Mary"; // string
+user = user + " Frunzete";
+
+var Skills = [`html`, `css`, `js`];
+
+var dan = {
+  age: 26,
+  employed: true,
+};
+var gulia = {
+  age: 25,
+  employed: true,
+};
+
+var jobTitle = document.getElementById("job-title");
