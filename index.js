d62b04d1b37eb13e9e090a5bf416d6bfe72fc5b4
Mary-Ann Johanna Frunzete <maryannfrunzete@gmail.com>
2023-01-12 19:45:53 +0200

    forEach using inline function (unonymous)

--- index.js
+++ index.js
@@ -12,16 +12,10 @@ skillsEl.innerHTML = skillsHTML;
 
 var pageIds = ["home", "skills", "languages", "projects"];
 
-function hidePage(id) {
-  hide("id");
-}
-
 function hideAllPages() {
-  pageIds.forEach(hidePage);
-  // hide("home");
-  // hide("skills");
-  // hide("languages");
-  // hide("projects");
+  pageIds.forEach(function (pageId) {
+    hide(pageId);
+  });
 }
 
 function show(id) {
