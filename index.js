07a65304ccb1510e1e3d4fc26cd19420c31fd3b7
Mary-Ann Johanna Frunzete <maryannfrunzete@gmail.com>
2023-01-26 20:20:31 +0200

    add fetch request to skills json

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 var skillsEl = document.getElementById("skills-list");
 
-var skills = [
-  { name: "HTML", endorcements: 5 },
-  { name: "CSS", endorcements: 4 },
-  { name: "JS", endorcements: 4 },
-  { name: "Teamcenter", endorcements: 9 },
-  { name: "BMIDE", endorcements: 6 },
-];
-// var skills = [["HTML", 5], ["CSS", 3], "JS", "Drive"];
+var skills = [];
 
-var skillsHTML = skills.map(function (skill) {
-  return `<li>${skill.name} - <span>${skill.endorcements}</span></li>`;
+var r1 = fetch("skills.json");
+r1.then(function (raspuns) {
+  var r2 = raspuns.json();
+  r2.then(function (skills) {
+    displaySkills(skills);
+  });
 });
 
-skillsEl.innerHTML = skillsHTML.join("");
+function displaySkills(skills) {
+  var skillsHTML = skills.map(function (skill) {
+    return `<li>${skill.name} - <span>${skill.endorcements}</span></li>`;
+  });
+
+  skillsEl.innerHTML = skillsHTML.join("");
+}
 
 var pageIds = ["home", "skills", "languages", "projects"];
 
